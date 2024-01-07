22bd430200fb9c53e14cc2f04e25e44b4c461728
hellsingglockboyz <vampirezarereal@gmail.com>
2024-01-07 14:57:14 -0300

    feat: 🎸 general

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <a href='https://www.youtube.com/@VAMPIRESAREREAL'>
+      <img src="imagem_site.png" alt="logo"  />
+        </a>
+    </div>
+  )
+}
+
+export default App
